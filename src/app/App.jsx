70f287720e4c03ae78dbf1546aca9c5c9ca1d8de
Ntavigwa Bashombe <ntavigwabashombe@gmail.com>
2022-05-25 18:04:07 +0300

    ft(setup):  Project Initialization

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='bg-gray-50 h-screen w-screen'>
+      <p className='text-lg'>App</p>
+    </div>
+  );
+};
+
+export default App;
